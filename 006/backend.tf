4c141cc2fea3919058403fb209b5c2a455bab490
Leonardo Gualpa <lgualpa.dev@gmail.com>
2025-10-02 11:40:20 -0500

    (debug) update configs

--- 006/backend.tf
+++ 006/backend.tf
@@ -4,12 +4,16 @@ terraform {
     key                  = "terraform.tfstate"
     region               = "us-east-1"
     workspace_key_prefix = "workspaces"
+    access_key           = "fake-test"
+    secret_key           = "fake-test"
 
     endpoints = {
       s3 = "http://localhost:4566"
     }
+
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
     use_path_style              = true
   }
 
