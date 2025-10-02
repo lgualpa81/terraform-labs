e86d19f875d9dddacf1ff9c24e2d3bfd4efeead9
Leonardo Gualpa <lgualpa.dev@gmail.com>
2025-10-02 12:00:54 -0500

    (debug) fix config

--- 006/backend.tf
+++ 006/backend.tf
@@ -13,8 +13,8 @@ terraform {
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    use_path_style              = true
+    #skip_requesting_account_id  = true
+    use_path_style = true
   }
 
 }
